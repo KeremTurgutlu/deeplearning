02da2632c82b22affd8a2c468fa5631f82a41dff
keremturgutlu <keremturgutlu@gmail.com>
2018-02-12 19:00:59 -0800

    unet

---
+++ datasciencebowl2018/model/loss.py
@@ -0,0 +1,10 @@
+# DICE LOSS
+def dice_loss(input, target):
+    smooth = 0
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+
+    return 1 - ((2. * intersection + smooth) /
+                (iflat.sum() + tflat.sum() + smooth))
