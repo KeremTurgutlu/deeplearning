3f942196cf38ec7d613696939c129af6c2072354
keremturgutlu <keremturgutlu@gmail.com>
2018-02-18 00:35:35 -0800

    save model

--- datasciencebowl2018/training/classifier.py
+++ datasciencebowl2018/training/classifier.py
@@ -36,24 +36,6 @@ class NucleiClassifier:
         self.epoch_counter = 0
         self.use_cuda = torch.cuda.is_available()
 
-    def restore_model(self, model_path, optim_path=None):
-        """
-        Restore a model parameters from the given path
-        Inputs:
-            model_path (str): The path to the model to restore
-        """
-        self.net.load_state_dict(torch.load(model_path))
-        if optim_path is not None:
-            self.optimizer.load_state_dict(optim_path)
-
-    def save_model(self, model_path, optim_path):
-        torch.save(self.net.state_dict(), model_path)
-        torch.save({
-            'optimizer': self.optimizer.state_dict(),
-            'iter': i,
-            'epoch': self.epoch_counter,
-        }, optim_path)
-
     def _criterion(self, logits, labels):
         return BCELoss2d().forward(logits, labels) + \
                SoftDiceLoss().forward(logits, labels)
@@ -137,7 +119,7 @@ class NucleiClassifier:
         print(f"Training : [{round(train_loss, 4)} , {round(train_dice_coeff, 4)}],\
 Validation : [{round(val_loss, 4)} , {round(val_dice_coeff, 4)}]")
 
-    # main functions
+    # main methods
     def train(self, train_loader, valid_loader, optimizer, epochs, threshold=0.5):
         self.optimizer = optimizer
         if self.use_cuda:
@@ -145,6 +127,24 @@ Validation : [{round(val_loss, 4)} , {round(val_dice_coeff, 4)}]")
 
         for epoch in range(epochs):
             self._run_epoch(train_loader, valid_loader, optimizer, threshold)
+
+
+    def restore_model(self, model_path, optim_path=None):
+        """
+        Restore a model parameters from the given path
+        Inputs:
+            model_path (str): The path to the model to restore
+        """
+        self.net.load_state_dict(torch.load(model_path))
+        if optim_path is not None:
+            self.optimizer.load_state_dict(optim_path)
+
+    def save_model(self, model_path, optim_path):
+        torch.save(self.net.state_dict(), model_path)
+        torch.save({
+            'optimizer': self.optimizer.state_dict(),
+            'epoch': self.epoch_counter
+        }, optim_path)
 
     def predict(self, test_loader):
         # eval mode
@@ -166,14 +166,3 @@ Validation : [{round(val_loss, 4)} , {round(val_dice_coeff, 4)}]")
                 preds.append(probs)
         return preds
 
-def show_predictions(dataloader, classifier, threshold=0.5):
-    print('\t\t Image \t\t\t\t\t Mask \t\t\t\t Predicted Mask')
-    for img, msk, _ in iter(dataloader):
-        plt.figure(figsize=(20, 20))
-        out = classifier.net(V(img))
-        plt.subplot(1, 3, 1)
-        plt.imshow(img.numpy()[0].transpose(1, 2, 0))
-        plt.subplot(1, 3, 2)
-        plt.imshow(msk.numpy()[0, 0])
-        plt.subplot(1, 3, 3)
-        plt.imshow((F.sigmoid(out).data.numpy()[0, 0] > threshold) * 1)
