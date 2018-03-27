da947cb7fdd497947e6321dafd3afac20cc24342
keremturgutlu <keremturgutlu@gmail.com>
2018-03-27 11:49:00 -0700

    update classifier + EDA

--- datasciencebowl2018/dataset/transform.py
+++ datasciencebowl2018/dataset/transform.py
@@ -1,46 +1,49 @@
 import cv2
 import random
 import numpy as np
+from scipy.ndimage.interpolation import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
 
-# Note on TTA
-# IN ORDER TO APPLY TTA WE NEED TO KNOW WHICH TRANSFORMS
-# WE ARE DOING ON THE TEST DATA SO THAT WE CAN RE-NORMALIZE IT
-# ANOTHER NOTE IS THAT WE NEED TO RESIZE THE LABELS BACK
+
+######################################
+####      2D TRANSFORMS          ####
+#####################################
+
 
 # resize
 def fix_resize_transform2(image, mask, w, h):
-    image = cv2.resize(image,(w,h))
-    mask  = cv2.resize(mask,(w,h))
-    mask  = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)[1]
+    image = cv2.resize(image, (w, h))
+    mask = cv2.resize(mask, (w, h))
+    mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)[1]
     return image, mask
 
 def fix_resize_transform(image, w, h):
-    image = cv2.resize(image,(w,h))
+    image = cv2.resize(image, (w, h))
     return image
 
 # horizontal flip
 def random_horizontal_flip_transform2(image, mask, p=0.5):
     if random.random() < p:
-        image = cv2.flip(image,1)
-        mask = cv2.flip(mask,1)
+        image = cv2.flip(image, 1)
+        mask = cv2.flip(mask, 1)
     return image, mask
 
 def random_horizontal_flip_transform(image, p=0.5):
     if random.random() < p:
-        image = cv2.flip(image,1)
+        image = cv2.flip(image, 1)
     return image
 
 
 # vertical flip
 def random_vertical_flip_transform2(image, mask, p=0.5):
     if random.random() < p:
-        image = cv2.flip(image,0)
-        mask = cv2.flip(mask,0)
+        image = cv2.flip(image, 0)
+        mask = cv2.flip(mask, 0)
     return image, mask
 
 def random_vertical_flip_transform(image, p=0.5):
     if random.random() < p:
-        image = cv2.flip(image,0)
+        image = cv2.flip(image, 0)
     return image
 
 
@@ -48,7 +51,7 @@ def random_vertical_flip_transform(image, p=0.5):
 def random_rotate_transform2(image, mask, p=0.5):
     H, W = mask.shape[0], mask.shape[1]
     if random.random() < p:
-        angle=random.randint(1,3)*90
+        angle = random.randint(1, 3)*90
         M = cv2.getRotationMatrix2D((H/2, W/2), angle, 1)
         image = cv2.warpAffine(image, M, (H, W))
         mask = cv2.warpAffine(mask, M, (H, W))
@@ -57,7 +60,7 @@ def random_rotate_transform2(image, mask, p=0.5):
 def random_rotate_transform(image, p=0.5):
     H, W = image.shape[0], image.shape[1]
     if random.random() < p:
-        angle=random.randint(1,3)*90
+        angle = random.randint(1, 3)*90
         M = cv2.getRotationMatrix2D((H/2, W/2), angle, 1)
         image = cv2.warpAffine(image, M, (H, W))
     return image
@@ -70,6 +73,7 @@ def random_rotate_angle_transform2(image, mask, p=0.5):
         M = cv2.getRotationMatrix2D((H/2, W/2), angle, 1)
         image = cv2.warpAffine(image, M, (H, W))
         mask = cv2.warpAffine(mask, M, (H, W))
+        mask = cv2.threshold(mask, 128, 215, cv2.THRESH_BINARY)[1]
     return image, mask
 
 def random_rotate_angle_transform(image, p=0.5):
@@ -104,9 +108,9 @@ def random_crop_resize2(image, mask, p=0.5):
         else:
             image, mask = image, mask
 
-        image = cv2.resize(image,(W,H))
-        mask  = cv2.resize(mask,(W,H))
-        mask  = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)[1]
+        image = cv2.resize(image, (W, H))
+        mask = cv2.resize(mask, (W, H))
+        mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)[1]
     return image, mask
 
 
@@ -132,63 +136,206 @@ def random_crop_resize(image, p=0.5):
             image = image
 
         image = cv2.resize(image, (W, H))
-        mask = cv2.resize(mask, (W, H))
-        mask = cv2.threshold(mask, 128, 255, cv2.THRESH_BINARY)[1]
-    return image, mask
+    return image
 
 
-# transforming 2d mask to multichannel
-def get_3d_mask(mask):
-    if len(np.unique(mask)) == 2:
-        back_channel = (mask == 30)*1 # background
-        nuclei_channel = (mask == 215)*1 # nuclei
-        overlap_channel = np.zeros_like(mask) # overlap - missing so all 0s
+
+######################################
+####      3D TRANSFORMS          ####
+#####################################
+
+def fix_resize_3D_transform2(image_voxel, mask_voxel, w, h):
+    image_voxel = np.array([cv2.resize(slice_, (w, h)) for slice_ in image_voxel])
+    mask_voxel = np.array([cv2.resize(slice_, (w, h)) for slice_ in mask_voxel])
+    # assign 215 to every pixel greater than 128
+    mask_voxel = cv2.threshold(mask_voxel.astype(np.uint8), 128, 215, cv2.THRESH_BINARY)[1]
+    return image_voxel, mask_voxel
+
+def fix_resize_3D_transform(image_voxel, w, h):
+    image_voxel = np.array([cv2.resize(slice_, (w, h)) for slice_ in image])
+    return image_voxel
+
+# horizontal flip 3D
+def random_horizontal_flip_3D_transform2(image_voxel, mask_voxel, p=0.5):
+    if random.random() < p:
+        image_voxel = np.array([cv2.flip(slice_, 1) for slice_ in image_voxel])
+        mask_voxel = np.array([cv2.flip(slice_, 1) for slice_ in mask_voxel])
+    return image_voxel, mask_voxel
+
+def random_horizontal_flip3D_transform(image_voxel, p=0.5):
+    if random.random() < p:
+        image_voxel = np.array([cv2.flip(slice_, 1) for slice_ in image_voxel])
+    return image_voxel
+
+
+# vertical flip 3D
+def random_vertical_flip_3D_transform2(image_voxel, mask_voxel, p=0.5):
+    if random.random() < p:
+        image_voxel = np.array([cv2.flip(slice_, 0) for slice_ in image_voxel])
+        mask_voxel = np.array([cv2.flip(slice_, 0) for slice_ in mask_voxel])
+    return image_voxel, mask_voxel
+
+def random_vertical_flip_3D_transform(image_voxel, p=0.5):
+    if random.random() < p:
+        image_voxel = np.array([cv2.flip(slice_, 0) for slice_ in image_voxel])
+    return image_voxel
+
+
+# 90, 180, 270 rotate 3D
+def random_rotate_3D_transform2(image_voxel, mask_voxel, p=0.5):
+    H, W = mask_voxel.shape[-1], mask_voxel.shape[-2]
+    if random.random() < p:
+        angle = random.randint(1, 3) * 90
+        M = cv2.getRotationMatrix2D((H / 2, W / 2), angle, 1)
+        image_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in image_voxel])
+        mask_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in mask_voxel])
+    return image_voxel, mask_voxel
+
+def random_rotate_3D_transform(image_voxel, p=0.5):
+    H, W = image_voxel.shape[-1], image_voxel.shape[-2]
+    if random.random() < p:
+        angle = random.randint(1, 3) * 90
+        M = cv2.getRotationMatrix2D((H / 2, W / 2), angle, 1)
+        image_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in image_voxel])
+    return image_voxel
+
+
+# range(0, 360, 10) angle rotate
+def random_rotate_angle_3D_transform2(image_voxel, mask_voxel, p=0.5):
+    angle = np.random.choice(range(0, 360, 10))
+    H, W = image_voxel.shape[-1], image_voxel.shape[-2]
+    if random.random() < p:
+        M = cv2.getRotationMatrix2D((H/2, W/2), angle, 1)
+        image_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in image_voxel])
+        mask_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in mask_voxel])
+    mask_voxel = np.array([cv2.threshold(slice_, 128, 215, cv2.THRESH_BINARY)[1] for slice_ in mask_voxel])
+    return image_voxel, mask_voxel
+
+def random_rotate_angle_3D_transform(image_voxel, p=0.5):
+    angle = np.random.choice(range(0, 360, 10))
+    H, W = image_voxel.shape[-1], image_voxel.shape[-2]
+    if random.random() < p:
+        M = cv2.getRotationMatrix2D((H/2, W/2), angle, 1)
+        image_voxel = np.array([cv2.warpAffine(slice_, M, (H, W)) for slice_ in image_voxel])
+    return image_voxel
+
+# RANDOM crop resize
+
+def random_crop_3D_resize2(image_voxel, mask_voxel, p=0.5):
+    H, W = image_voxel.shape[-1], image_voxel.shape[-2]
+    if random.random() < p:
+        # PICK A RANDOM POINT
+        j, i = np.random.choice(range(H)), np.random.choice(range(W))
+
+        # UPPER LEFT
+        if (j <= H / 2) & (i <= W / 2):
+            image_voxel = np.array([slice_[j:H, i:H] for slice_ in image_voxel])
+            mask_voxel = np.array([slice_[j:H, i:H] for slice_ in mask_voxel])
+
+        # LOWER LEFT
+        elif (j >= H / 2) & (i <= W / 2):
+            image_voxel = np.array([slice_[:j, i:H] for slice_ in image_voxel])
+            mask_voxel = np.array([slice_[:j, i:H] for slice_ in mask_voxel])
+
+        # UPPER RIGHT
+        elif (j <= H / 2) & (i >= W / 2):
+            image_voxel = np.array([slice_[j:H, :i] for slice_ in image_voxel])
+            mask_voxel = np.array([slice_[j:H, :i] for slice_ in mask_voxel])
+
+        # LOWER RIGHT
+        elif (j >= H / 2) & (i >= W / 2):
+            image_voxel = np.array([slice_[:j, :i] for slice_ in image_voxel])
+            mask_voxel = np.array([slice_[:j, :i] for slice_ in mask_voxel])
+
+        else:
+            image_voxel, mask_voxel = image_voxel, mask_voxel
+
+        image_voxel = np.array([cv2.resize(slice_, (W, H)) for slice_ in image_voxel])
+        mask_voxel = np.array([cv2.resize(slice_, (W, H)) for slice_ in mask_voxel])
+        mask_voxel = np.array([cv2.threshold(slice_, 128, 215, cv2.THRESH_BINARY)[1] for slice_ in mask_voxel])
+    return image_voxel, mask_voxel
+
+
+def random_crop_3D_resize(image_voxel, p=0.5):
+    H, W = image_voxel.shape[-1], image_voxel.shape[-2]
+    if random.random() < p:
+        # PICK A RANDOM POINT
+        j, i = np.random.choice(range(H)), np.random.choice(range(W))
+
+        # UPPER LEFT
+        if (j <= H / 2) & (i <= W / 2):
+            image_voxel = np.array([slice_[j:H, i:H] for slice_ in image_voxel])
+            # LOWER LEFT
+        elif (j >= H / 2) & (i <= W / 2):
+            image_voxel = np.array([slice_[:j, i:H] for slice_ in image_voxel])
+
+        # UPPER RIGHT
+        elif (j <= H / 2) & (i >= W / 2):
+            image_voxel = np.array([slice_[j:H, :i] for slice_ in image_voxel])
+
+        # LOWER RIGHT
+        elif (j >= H / 2) & (i >= W / 2):
+            image_voxel = np.array([slice_[:j, :i] for slice_ in image_voxel])
+
+        else:
+            image_voxel = image_voxel
+
+        image_voxel = np.array([cv2.resize(slice_, (W, H)) for slice_ in image_voxel])
+    return image_voxel
+
+
+def elastic_transform(image, alpha, sigma, alpha_affine, random_state=None):
+    """Elastic deformation of images as described in [Simard2003]_ (with modifications).
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+         Convolutional Neural Networks applied to Visual Document Analysis", in
+         Proc. of the International Conference on Document Analysis and
+         Recognition, 2003.
+
+     Based on https://gist.github.com/erniejunior/601cdf56d2b424757de5
+    """
+    if random_state is None:
+        random_state = np.random.RandomState(None)
     else:
-        back_channel = (mask == 30)*1 # background
-        nuclei_channel = (mask == 110)*1 # nuclei
-        overlap_channel = (mask == 215)*1 # overlap
-    # stack depth-wise
-    multiclass_mask = np.dstack([back_channel, nuclei_channel, overlap_channel])
-    return multiclass_mask
+        random_state = np.random.RandomState(random_state)
+
+    shape = image.shape
+    shape_size = shape[:2]
+
+    # Random affine
+    center_square = np.float32(shape_size) // 2
+    square_size = min(shape_size) // 3
+    pts1 = np.float32([center_square + square_size, [center_square[0] + square_size, center_square[1] - square_size],
+                       center_square - square_size])
+    pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine, size=pts1.shape).astype(np.float32)
+    M = cv2.getAffineTransform(pts1, pts2)
+    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+    dz = np.zeros_like(dx)
+
+    x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
+    indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
+
+    return map_coordinates(image, indices, order=1, mode='reflect').reshape(shape)
 
 
-
-# ELASTIC TRANSFORMATION
-
-# import numpy as np
-# from scipy.ndimage.interpolation import map_coordinates
-# from scipy.ndimage.filters import gaussian_filter
-#
-# def elastic_transform(image, alpha, sigma, random_state=None):
-#     """Elastic deformation of images as described in [Simard2003]_.
-#     .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
-#        Convolutional Neural Networks applied to Visual Document Analysis", in
-#        Proc. of the International Conference on Document Analysis and
-#        Recognition, 2003.
-#     """
-#     if random_state is None:
-#         random_state = np.random.RandomState(None)
-#
-#     shape = image.shape
-#     dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-#     dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma, mode="constant", cval=0) * alpha
-#     dz = np.zeros_like(dx)
-#
-#     x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]))
-#     indices = np.reshape(y+dy, (-1, 1)), np.reshape(x+dx, (-1, 1)), np.reshape(z, (-1, 1))
-#
-#     distored_image = map_coordinates(image, indices, order=1, mode='reflect')
-#     return distored_image.reshape(image.shape)
-#
-#
-#
-#
-#
-#
-#
-
-
-
-
-
-
+def random_elastic_3D_transform2(image_voxel, mask_voxel, p=0.5):
+    if random.random() < p:
+        random_state = np.random.randint(1)
+        new_image_voxel, new_mask_voxel = [], []
+        # Merge images into separete channels (shape will be (cols, rols, 2))
+        for im, im_mask in zip(image_voxel, mask_voxel):
+            im_merge = np.concatenate((im[..., None], im_mask[..., None]), axis=2)
+            im_merge_t = elastic_transform(image=im_merge,
+                                           alpha=im_merge.shape[1] * 2,
+                                           sigma=im_merge.shape[1] * 0.1,
+                                           alpha_affine=im_merge.shape[1] * 0.01,
+                                           random_state=random_state)
+            # Split image and mask
+            im_t = im_merge_t[..., 0]
+            im_mask_t = im_merge_t[..., 1] > 0
+            new_image_voxel.append(im_t)
+            new_mask_voxel.append(im_mask_t)
+        image_voxel, mask_voxel = np.array(new_image_voxel), np.array(new_mask_voxel)
+    return image_voxel, mask_voxel
--- datasciencebowl2018/training/classifier.py
+++ datasciencebowl2018/training/classifier.py
@@ -1,7 +1,7 @@
 import torch
 from torch.autograd import Variable as V
 import torch.nn.functional as F
-from model.loss import *
+from evals.loss import *
 import matplotlib.pyplot as plt
 
 # Average meter counter
@@ -22,126 +22,130 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
+
 class NucleiClassifier:
-    def __init__(self, net, max_epochs):
+    def __init__(self, net, optimizer, crit, metric, gpu=1):
         """
         Neural Net classifier for Data Science Bowl
         Inputs:
             net (nn.Module): PyTorch model that is going to forward
-            max_epochs (int) : The max number of epochs to train the model
         """
 
         self.net = net
-        self.max_epochs = max_epochs
-        self.epoch_counter = 0
-        self.use_cuda = torch.cuda.is_available()
-
-    # def _criterion(self, logits, labels):
-    #     return BCELoss2d().forward(logits, labels) + \
-    #            SoftDiceLoss().forward(logits, labels)
+        self.epoch_counter = 0  # counts number of epochs
+        self.use_cuda = torch.cuda.is_available()  # enable cuda if gpu available
+        self.gpu = gpu  # gpu id to train on
+        self.optimizer = optimizer  # optimizer to use
+        self.crit = crit  # loss function to use
+        self.metric = metric  # metric to use
 
     def _validate_epoch(self, valid_loader, threshold):
         """
         Validate by self._criterion
         """
-        losses = AverageMeter() #reset for epoch
-
+        losses = AverageMeter()  # reset for epoch
+        metrics = AverageMeter()  # reset for epoch
         batch_size = valid_loader.batch_size
 
         for ind, (inputs, targets, index) in enumerate(valid_loader):
             if self.use_cuda:
-                inputs = inputs.cuda()
-                targets = targets.cuda()
-            inputs, targets = V(inputs, volatile=True), V(targets, volatile=True) # volatile since inference mode
+                inputs = inputs.cuda(self.gpu)
+                targets = targets.cuda(self.gpu)
+            inputs, targets = V(inputs, volatile=True), V(targets, volatile=True)  # volatile since inference mode
 
             # forward
             logits = self.net(inputs)
-            # compute loss for iteration
-            loss = self._criterion(logits, targets)
-            # update loss for iteration
+
+            # compute and update loss
+            loss = self.crit(logits, targets)
             losses.update(loss.data[0], batch_size)
 
-        return losses.avg
+            # compute and update metric
+            metric = self.metric(logits, targets, threshold)
+            metrics.update(metric.data[0], batch_size)
+
+        return losses.avg, metrics.avg
 
     def _train_epoch(self, train_loader, optimizer, threshold):
         """
         Optimize by self._criterion and self.optimizer
         """
-        losses = AverageMeter() # reset for epoch
+        losses = AverageMeter()  # reset for epoch
+        metrics = AverageMeter()  # reset for epoch
         batch_size = train_loader.batch_size
 
         for ind, (inputs, targets, index) in enumerate(train_loader):
 
             if self.use_cuda:
-                inputs = inputs.cuda()
-                targets = targets.cuda()
+                inputs = inputs.cuda(self.gpu)
+                targets = targets.cuda(self.gpu)
             inputs, targets = V(inputs), V(targets)
 
             # forward
             logits = self.net(inputs)
-            # compute loss for iteration
-            loss = self._criterion(logits, targets)
-            # update loss for iteration
+
+            # compute and update loss
+            loss = self.crit(logits, targets)
             losses.update(loss.data[0], batch_size)
-            # update weights
+
+            # compute and update metric
+            metric = self.metric(logits, targets, threshold)
+            metrics.update(metric.data[0], batch_size)
+
+            # backprop - update weights
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        return losses.avg
+        return losses.avg, metrics.avg
 
-    def _run_epoch(self, train_loader, valid_loader, optimizer,
-                   threshold=0.5):
+    def _run_epoch(self, train_loader, valid_loader, threshold=0.5):
         """
         Run a single epoch print training and validation loss
         """
-        # train mode
+        # train mode and run a train pass
         self.net.train()
+        train_loss, train_metric = self._train_epoch(train_loader, self.optimizer, threshold)
 
-        # run a train pass
-        train_loss = self._train_epoch(train_loader, optimizer, threshold)
-
-        # eval mode
+        # eval mode and validate epoch
         self.net.eval()
-
-        # validate epoch
-        val_loss = self._validate_epoch(valid_loader, threshold)
-
+        val_loss, val_metric = self._validate_epoch(valid_loader, threshold)
         self.epoch_counter += 1
 
         print(f"Epoch: {self.epoch_counter}")
-        print(f"Training : [{round(train_loss, 4)}],\
-Validation : [{round(val_loss, 4)}]")
+        print(f"LOSS - Training : [{round(train_loss, 4)}], Validation : [{round(val_loss, 4)}]")
+        print(f"METRIC - Training : [{round(train_metric, 4)}], Validation : [{round(val_metric, 4)}]")
 
     # main methods
-    def train(self, train_loader, valid_loader, optimizer, crit, epochs, threshold=0.5):
+    def train(self, train_loader, valid_loader, epochs, threshold=0.5):
         """
-        Takes optimizer and criterion
+        ...
         """
-        self.optimizer = optimizer
-        self._criterion = crit
-
         if self.use_cuda:
-            self.net.cuda()
+            self.net.cuda(self.gpu)
 
         for epoch in range(epochs):
-            self._run_epoch(train_loader, valid_loader, optimizer, threshold)
+            self._run_epoch(train_loader, valid_loader, threshold)
 
-
-    def restore_model(self, model_path, optim_path=None):
+    def restore_model(self, path, gpu):
         """
         Restore a model parameters from the given path
         Inputs:
             model_path (str): The path to the model to restore
         """
-        self.net.load_state_dict(torch.load(model_path))
-        if optim_path is not None:
-            self.optimizer.load_state_dict(optim_path)
+        self.net.cpu()
+        state = torch.load(path)
+        self.net.load_state_dict(state['state_dict'])
+        self.optimizer.load_state_dict(state['optimizer'])
+        self.epoch_counter = state['epoch']  # counts number of epochs
 
-    def save_model(self, model_path, optim_path):
-        torch.save(self.net.state_dict(), model_path)
-        if optim_path is not None:
-            torch.save(self.optimizer.state_dict(), optim_path)
+    def save_model(self, path):
+        state = {
+            'epoch': self.epoch_counter,
+            'state_dict': self.net.state_dict(),
+            'optimizer': self.optimizer.state_dict()
+        }
+        torch.save(state, path)
 
     def predict(self, test_loader):
         # eval mode
@@ -149,7 +153,7 @@ Validation : [{round(val_loss, 4)}]")
         preds = []
         for ind, (images, index) in enumerate(test_loader):
             if self.use_cuda:
-                images = images.cuda()
+                images = images.cuda(self.gpu)
 
             images = V(images, volatile=True)
 
@@ -159,4 +163,3 @@ Validation : [{round(val_loss, 4)}]")
             probs = probs.data.cpu().numpy()
             preds.append(probs)
         return preds
-
